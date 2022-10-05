ecf2e7f74883997fd285ecafa81312aeb52624ed
Caroline <anacdolima@gmail.com>
2022-10-05 19:45:10 -0300

    basic reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent implements OnInit {
+  // define um formGroup
+  clientForm = this.fb.group({
+    name: [''], // define formControl - pode ser this.fb.control('')
+    address: [''],
+    phones: this.fb.array(['']), // define um formArray
+
+    // formArray com objeto
+    // inicia vazio
+    dependents: this.fb.array([]),
+    // inicia com um elemento
+    // dependents: this.fb.array([
+    //   this.fb.group({
+    //     name: [''],
+    //     age: [''],
+    //   }),
+    // ]),
+  });
+
+  // listas (arrays) de telefone e dependentes
+  phones = this.clientForm.get('phones') as FormArray;
+  dependents = this.clientForm.get('dependents') as FormArray;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  addEmployee() {
+    console.log(this.clientForm.value);
+    console.log(this.dependents.controls);
+  }
+
+  addPhone() {
+    this.phones.push(this.fb.control(''));
+  }
+
+  addChild() {
+    this.dependents.push(
+      this.fb.group({
+        name: [''],
+        age: [''],
+      })
+    );
+  }
+
+  removeChild(i: number) {
+    this.dependents.removeAt(i);
+  }
+}
